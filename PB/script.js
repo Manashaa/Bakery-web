6f2d15d52961c62ded0f6632c8d66c54030a5d1f
Manashaa <sewmini73karunarathne@gmail.com>
2025-09-01 21:14:53 +0530

    correct one

--- PB/script.js
+++
@@ -1,9 +0,0 @@
-alert("WELCOME TO PEOPLES BAKERS");
-
-function myMap() {
-var mapProp= {
-  center:new google.maps.LatLng(51.508742,-0.120850),
-  zoom:5,
-};
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
